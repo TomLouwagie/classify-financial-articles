5de32d70c828b65ba0f584cc13272fa603f962f8
Tom Louwagie <tom@louwagie.com>
2022-12-04 13:58:01 +0100

    First commit

---
+++ classify_financial_articles.py
@@ -0,0 +1,10 @@
+from gravityai import gravityai as grav
+import pickle
+import pandas as pd
+
+model = pickle.load(open(''))
+
+# Helps us count frequency of words
+tfidf_vectorizer = pickle.load(open(''))
+label_encoder = pickle.load(open(''))
+
